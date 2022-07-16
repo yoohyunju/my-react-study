010fdab9f1728a9287e5aaea347dee2ea16f1118
hazel <yhj2756@naver.com>
2022-07-16 20:58:55 +0900

    React 기초 및 JSX

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div>July 13th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
