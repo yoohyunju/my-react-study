108a392c6007fa92a9b7fc26cd94a6ad3beeac49
hazel <yhj2756@naver.com>
2022-07-21 00:00:02 +0900

    섹션 3

---
+++ 01-starting-setup/src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,18 @@
+import './ExpenseDate.css';
+
+const ExpenseDate = (props) => {
+  console.log(props)
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div> {/* Date 객체는 문자열로 변환 해주어야 출력 가능 */}
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
