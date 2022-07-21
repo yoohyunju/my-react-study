7f61a56ccfbf41a1a3c74d27b67186bc24a3155f
hazel <yhj2756@naver.com>
2022-07-21 22:45:36 +0900

    two-way binding

---
+++ section4/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  // 방법 1.
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  // 방법 2.
+  // const [userInput, serUserInput] = useState ({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: ""
+  // })
+
+  const titleChangeHandler = (event) => {
+    // 방법 1-1.
+    setEnteredTitle(event.target.value);
+
+    // 방법 2-1.
+    // serUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value // 오버라이딩
+    // });
+
+    // 방법 2-2.
+    /* 리액트는 상태 업데이트 스케줄을 갖고있어서 바로 실행하지 않음
+    이 방법이 더 안전함 */
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // })
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            nChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
